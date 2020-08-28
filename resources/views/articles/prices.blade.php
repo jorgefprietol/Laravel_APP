c6e50436b96b55b3cc392f6efb5365ac4f8cdf6d
Jorge Fidel Prieto Linares <jorgefprietol@gmail.com>
2020-08-28 12:03:05 -0500

    Integracion de bootstrap 4 y configuracion con las vistas

--- resources/views/articles/prices.blade.php
+++ resources/views/articles/prices.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row">
-    <h3>Precios de articulos desde articles.prices</h3>
-
-    <a href="{{URL('/')}}">Home</a>
-</div>
+    <div class="row">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="{{URL('/')}}" class="btn btn-primary">Home</a>
+    </div>
 @endsection
